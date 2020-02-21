8ca10ac63501ddf138080659a5db39901b5ab34b
serigne007mor <serignemortoure1@gmail.com>
2020-02-21 12:49:12 -0600

    first commit

---
+++ Assets/ClimberHand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Valve.VR;
+public class ClimberHand : MonoBehaviour
+{
+    public SteamVR_Input_Sources Hand;
+    public int TouchedCount;
+    public bool grabbing;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            TouchedCount++;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            TouchedCount--;
+        }
+    }
+}
