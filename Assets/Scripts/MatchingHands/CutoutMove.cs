51836273b8e08f1cd25cab33f1bb02ea5e0494c5
Samir Kashyap <samir.kashyap17@gmail.com>
2020-02-21 14:19:21 -0600

    Added basic movement for moving wall

---
+++ Assets/Scripts/MatchingHands/CutoutMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutoutMove : MonoBehaviour
+{
+    public float speed;
+    private float z = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        z = z - (speed/100f);
+        transform.position = new Vector3(0f, 1.5f, z);
+    }
+}
