7bf9dd8b8e9e0b0b83465a312dd24039e9726928
serigne007mor <serignemortoure1@gmail.com>
2020-02-27 19:46:01 -0600

    fixed switch scene teleport

--- Assets/SteamVR/InteractionSystem/Teleport/Scripts/TeleportPoint.cs
+++ Assets/SteamVR/InteractionSystem/Teleport/Scripts/TeleportPoint.cs
@@ -221,6 +221,7 @@ namespace Valve.VR.InteractionSystem
 		{
 			if ( !string.IsNullOrEmpty( switchToScene ) )
 			{
+				SteamVR_LoadLevel.Begin(switchToScene);
 				Debug.Log("<b>[SteamVR Interaction]</b> TeleportPoint: Hook up your level loading logic to switch to new scene: " + switchToScene, this);
 			}
 			else
