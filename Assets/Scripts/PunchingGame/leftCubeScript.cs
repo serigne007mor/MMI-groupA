d907a08e9d93f55108ecc54647c205cd283ba556
serigne007mor <serignemortoure1@gmail.com>
2020-03-02 15:58:51 -0600

    added the portals to the hub

--- Assets/Scripts/PunchingGame/leftCubeScript.cs
+++ Assets/Scripts/PunchingGame/leftCubeScript.cs
@@ -1,71 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR.InteractionSystem;
 using Valve.VR;
 
 public class leftCubeScript : MonoBehaviour {
-  
- 
-    public SteamVR_Action_Boolean destroyAction;
-    private Interactable interactable;
 
 
-    void Start()
-    {
-        interactable = GetComponent<Interactable>();
-    }
 
+    void OnTriggerEnter (Collider other) {
+        if (other.gameObject.CompareTag ("lefttHand")) {
+            gameObject.SetActive (false);
 
-    //void Update()
-    //{
-    //    if (interactable.attachedToHand != null)
-    //    {
-    //        SteamVR_Input_Sources source = interactable.attachedToHand.handType;
-
-    //        if (destroyAction[source].stateDown)
-    //        {
-    //            gameObject.SetActive(false);
-    //        }
-    //    }
-    //}
-
-    private void OnHandHoverBegin(Hand hand)
-    {
-        hand.ShowGrabHint();
-    }
-
-
-    private void OnHandHoverEnd(Hand hand)
-    {
-        hand.HideGrabHint();
-    }
-
-
-    private void OnHandHoverUpdate(Hand hand)
-    {
-        GrabTypes grabType = hand.GetGrabStarting();
-        bool isGrabEnding = hand.IsGrabEnding(gameObject);
-
-        if (interactable.attachedToHand == null && grabType != GrabTypes.None)
-        {
-            hand.AttachObject(gameObject, grabType);
-            hand.HoverLock(interactable);
-            hand.HideGrabHint();
         }
 
-        else if (isGrabEnding)
-        {
-            hand.DetachObject(gameObject);
-            hand.HoverLock(interactable);
-        }
     }
-
-    //void OnTriggerEnter (Collider other) {
-    //    if (other.gameObject.CompareTag ("lefttHand")) {
-    //        gameObject.SetActive (false);
-
-    //    }
-
-    //}
 }
