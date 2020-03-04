7d8a86f396e5a596dd06c8998d724cad1fded2de
Samir Kashyap <samir.kashyap17@gmail.com>
2020-03-04 16:55:23 -0600

    Added partial functionality for detecting when player passes the wall

--- Assets/Scripts/MatchingHands/CutoutMove.cs
+++ Assets/Scripts/MatchingHands/CutoutMove.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class CutoutMove : MonoBehaviour
 {
     public float speed;
+    public Progress progress;
     private float z = 6f;
+    private bool stop = false;
 
     // Update is called once per frame
     void Update()
     {
         z = z - (speed/100f);
         transform.position = new Vector3(0f, 1.5f, z);
+
+        if (z < -4)
+        {
+            if (stop == false) {
+                progress.pass();
+                stop = true;
+            }
+            
+        }
     }
 }
---
+++ Assets/Scripts/MatchingHands/Progress.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Progress : MonoBehaviour
+{
+    bool firstleft, firstright, secondleft, secondright, thirdleft, thirdright;
+    public GameObject first_cutout;
+    public GameObject second_cutout;
+    public GameObject third_cutout;
+    private GameObject firstcutout;
+    private GameObject secondcutout;
+    private GameObject thirdcutout;
+
+    void Start()
+    {
+        firstleft = false;
+        firstright = false;
+        secondleft = false;
+        secondright = false;
+        thirdleft = false;
+        thirdright = false;
+    }
+
+    public void increment(int number, string id)
+    {
+        if (firstleft == false && number > 5 && id == "left")
+        {
+            Debug.Log("first left pass");
+            firstleft = true;
+        }
+        else if (firstright == false && number > 5 && id == "right")
+        {
+            Debug.Log("first left pass");
+            firstright = true;
+
+        }
+    }
+
+    public void pass()
+    {
+        Debug.Log("Got into Pass");
+    }
+
+    void SpawnNew()
+    {
+        firstcutout = (GameObject)Instantiate(first_cutout);
+        secondcutout = (GameObject)Instantiate(second_cutout);
+        thirdcutout = (GameObject)Instantiate(third_cutout);
+    }
+}
