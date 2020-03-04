0d76d743809c6a3ab5e852e974a5eac927a55ee6
serigne007mor <serignemortoure1@gmail.com>
2020-03-04 15:39:09 -0600

    modified punching and hub

---
+++ Assets/Scripts/PunchingGame/Pacer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacer : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private float xMax = 7.5f;
+    private float xMin = -7.5f;
+    private int direction = 1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xNew = transform.position.x +
+        direction * speed * Time.deltaTime;
+        if (xNew >= xMax)
+        {
+            xNew = xMax;
+            direction *= -1;
+        }
+        else if (xNew <= xMin)
+        {
+            xNew = xMin;
+            direction *= -1;
+        }
+        transform.position = new Vector3(xNew, 0.35f, -0.3f);
+    }
+
+
+}
