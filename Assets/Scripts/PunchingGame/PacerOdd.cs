2c09dd35cfce9480f5a2d2f395bde87a2a201876
serigne007mor <serignemortoure1@gmail.com>
2020-03-29 18:01:23 -0500

    added 2 gameplays to punching game and made the babies move for added difficulty

--- Assets/Scripts/PunchingGame/PacerOdd.cs
+++ Assets/Scripts/PunchingGame/PacerOdd.cs
@@ -2,33 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pacer : MonoBehaviour
+public class PacerOdd : MonoBehaviour
 {
     public float min = 0f;
     public float max = 0f;
+    
     // Use this for initialization
     void Start()
     {
-
+        //startTime = Time.time;
         min = transform.position.x - 1;
         max = transform.position.x;
+     
 
     }
-        IEnumerator Wait(float duration)
-    {
-        //This is a coroutine
-       Debug.Log("Start Wait() function. The time is: "+Time.time);
-        Debug.Log( "Float duration = "+duration);
-         yield return new WaitForSeconds(duration);   //Wait
-        Debug.Log("End Wait() function and the time is: "+Time.time);
-    }
-    // Update is called once per frame
     void Update()
     {
-        if ((Time.time) % 2 > 0.9)
+        
+        if((Time.time) % 2 < 0.9)
             transform.position = new Vector3(Mathf.PingPong((Time.time) * 2, max - min) + min, transform.position.y, transform.position.z);
 
     }
 
-
+    
 }
--- Assets/Scripts/PunchingGame/rightCubeScript.cs
+++ Assets/Scripts/PunchingGame/rightCubeScript.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class rightCubeScript : MonoBehaviour
 {
     public string destroyer;
-
+    public TextMesh score;
     void OnTriggerEnter(Collider other)
         {
             if(other.gameObject.CompareTag(destroyer))
             {
                 gameObject.SetActive(false);
-                
-            }
+                score.text = (int.Parse(score.text) + 1).ToString();
+
+        }
 
 
         }
